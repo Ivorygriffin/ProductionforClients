e13de2fbec85db05d4a426cab2cb81c016bec84d
KobiAndtAIE <s210291@students.aie.edu.au>
2022-06-06 09:31:43 +0930

    fixed conflict

--- Production for Clients/Assets/n a r r a t i v e/textbox.cs
+++ Production for Clients/Assets/n a r r a t i v e/textbox.cs
@@ -10,15 +10,14 @@ public class textbox : MonoBehaviour
     public TMP_Text dialogue;
     public int currentArrayNum;
     public GameObject dialogueBox;
-<<<<<<< HEAD
+
     public GameObject cutcam;
     public GameObject nextLevelTrigger;
     
 
     public Parkour parkourScript;
     public PlayerController playerController;
-=======
->>>>>>> KOBI-DEFINITIVE-BRANCH
+
 
     public void Start()
     {
@@ -35,14 +34,12 @@ public class textbox : MonoBehaviour
         if(currentArrayNum == texts.Length)
         {
             dialogueBox.SetActive(false);
-<<<<<<< HEAD
+
             parkourScript.enabled = true;
             playerController.enabled = true;
             cutcam.SetActive(false);
             nextLevelTrigger.SetActive(true);
-            
-=======
->>>>>>> KOBI-DEFINITIVE-BRANCH
+
         }
     }
 }
